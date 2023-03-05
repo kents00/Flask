01365d3c4a321c33c11f4e021b0898b40d322dc4
Kent Edoloverio <69900896+kents00@users.noreply.github.com>
2023-03-05 14:47:19 +0800

    March 5, 2023

---
+++ src/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='static')
+app.secret_key = "your secret key"
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
